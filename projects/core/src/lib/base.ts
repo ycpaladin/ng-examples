66d2bb9c3621f3c0272b299433c849dcde6a38b3
chenke <chenke@cyai.com>
2022-04-07 02:22:36 +0000

    docs():清理无效的代码

--- projects/core/src/lib/base.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-import { SafeType, UserModel } from './interfaces';
-
-
-export abstract class User<T extends UserModel = UserModel> extends Observable<T> {
-  abstract login(username: string, password: string, extra?: SafeType): void;
-  abstract logout(): void;
-}
